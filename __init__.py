113cfff57e4e95015563fae22f049c94c74cb8e9
zaqxs123456 <56287342+zaqxs123456@users.noreply.github.com>
2025-11-30 17:16:51 +0800

    Refactor ensure_file function to improve directory handling and ensure destination directory is created before file operations

--- __init__.py
+++ __init__.py
@@ -16,19 +16,22 @@ TEMPLATE_PATH = "data/project_templates.yaml"
 from utils.logger_util.logger import Logger
 logger = Logger(name="ProjectCreatorInit")
 
+dest_dir = Path("./project/data/project_creator_core")
+if dest_dir.exists():
+    shutil.rmtree(dest_dir)
+dest_dir.mkdir(parents=True, exist_ok=True)
+
 def ensure_file(template_path: str) -> Path:
-    dest_dir = Path(f"./project/data/project_creator_core")
-    dest_dir.mkdir(parents=True, exist_ok=True)
     dest_file_name = Path(template_path).name
     dest_path = dest_dir / dest_file_name
-    if not dest_path.exists():
-        src = Path(__file__).parent / template_path
-        if not src.exists():
-            raise FileNotFoundError(f"Bundled template not found: {src}")
-        try:
-            shutil.copyfile(src, dest_path)
-        except Exception as e:
-            raise IOError(f"Failed to copy template to {dest_path}: {e}") from e
+
+    src = Path(__file__).parent / template_path
+    if not src.exists():
+        raise FileNotFoundError(f"Bundled template not found: {src}")
+    try:
+        shutil.copyfile(src, dest_path)
+    except Exception as e:
+        raise IOError(f"Failed to copy template to {dest_path}: {e}") from e
     logger.info(f"{template_path} ensured at: {dest_path}")
     return dest_path
 
